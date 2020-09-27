40465a0b73702ff2d99f89022c552842dd7db6eb
Cameron Cousins <cameroncousins18@gmail.com>
2020-09-27 21:45:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,61 +1,24 @@
-# require 'open-uri'
-# require 'pry'
-# 
-# class Scraper
-# 
-#   def self.scrape_index_page(index_url)
-#     
-#   end
-# 
-#   def self.scrape_profile_page(profile_url)
-#     
-#   end
-# 
-# end
-
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    page = Nokogiri::HTML(open(index_url))
-    students = []
+    # - should use Nokogiri and open-uri to access the index_url
+    # - reutrn value should be an array of hashes in which each hash represents a single student
+    # - The keys of the individual student hashes should be :name, :location and :profile_url.
 
-    page.css("div.student-card").each do |student|
-      name = student.css(".student-name").text
-      location = student.css(".student-location").text
-      profile_url = student.css("a").attribute("href").value
-      student_info = {:name => name,
-                :location => location,
-                :profile_url => profile_url}
-      students << student_info
-      end
-    students
-   end
-
+  end
 
   def self.scrape_profile_page(profile_url)
-      page = Nokogiri::HTML(open(profile_url))
-      student = {}
-
-      # student[:profile_quote] = page.css(".profile-quote")
-      # student[:bio] = page.css("div.description-holder p")
-      container = page.css(".social-icon-container a").collect{|icon| icon.attribute("href").value}
-      container.each do |link|
-        if link.include?("twitter")
-          student[:twitter] = link
-        elsif link.include?("linkedin")
-          student[:linkedin] = link
-        elsif link.include?("github")
-          student[:github] = link
-        elsif link.include?(".com")
-          student[:blog] = link
-        end
-      end
-      student[:profile_quote] = page.css(".profile-quote").text
-      student[:bio] = page.css("div.description-holder p").text
-      student
+    
   end
 
 end
+
+
+
+# - build a class that is responsible for scraping data from a web page 
+# - build a class that uses that data to instantiate new objects
+# - Understand the contents of a third class that is responsible for the command line interface
+# - Use meta-programming to instantiate new instances of a class and add attributes to instances of a class.
