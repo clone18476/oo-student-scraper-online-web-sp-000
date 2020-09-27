3f12e23538536a6fff29e5f3c312c100e2875edf
Cameron Cousins <cameroncousins18@gmail.com>
2020-09-27 21:53:16 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,6 +7,7 @@ class Scraper
     # - should use Nokogiri and open-uri to access the index_url
     # - reutrn value should be an array of hashes in which each hash represents a single student
     # - The keys of the individual student hashes should be :name, :location and :profile_url.
+    page = Nokogiri::HTML(open(index_url))
 
   end
 
